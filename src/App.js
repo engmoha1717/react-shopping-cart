b971bf6f754718e15f660968c641b9d054653f7b
ibrahim mohamed <engmoha1717@email.com>
2022-12-15 16:43:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './index.css'
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">react shopping cart</a>
+      </header>
+      <main>
+        product list
+      </main>
+      <footer>
+        all right is reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
